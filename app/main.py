29f93f43422834d604032ae47b5eaef76d91a381
komola-me <komolaziya28@gmail.com>
2025-09-03 18:08:22 +0500

    add: frontend

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.routers.auth import router as auth_router
 from app.routers.article import router as article_router
 from app.middleware import LoggingMiddleware
 from app.admin.settings import admin
 
-app = FastAPI(debug=True)
+
+app = FastAPI(debug=True, title="Medium Clone")
 
 app.add_middleware(LoggingMiddleware)
 app.add_middleware(
@@ -21,3 +25,13 @@ app.include_router(auth_router)
 app.include_router(article_router)
 
 admin.mount_to(app=app)
+
+app.mount('/static', StaticFiles(directory="app/static"), name="static")
+
+app.mount('/media', StaticFiles(directory="app/media"), name="media")
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def welcome_page(request: Request):
+    return templates.TemplateResponse("welcome.html", {"request": request})
